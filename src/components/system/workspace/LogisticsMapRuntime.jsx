83d28b469ba993aa384bebf324e6cbdb3b120af5
이관용 Lee Kwan Yong <10524@IGISAM.COM>
2026-07-10 15:10:50 +0900

    Keep Leaflet callouts inside map bounds

--- src/components/system/workspace/LogisticsMapRuntime.jsx
+++ src/components/system/workspace/LogisticsMapRuntime.jsx
@@ -32,7 +32,7 @@ export const MAP_CALLOUT_POSITION = Object.freeze({
   leaflet: Object.freeze({
     innerCentered: false,
     offset: Object.freeze([-16, -25]),
-    panPaddingTopLeft: Object.freeze([170, 96]),
+    panPaddingTopLeft: Object.freeze([170, 128]),
     panPaddingBottomRight: Object.freeze([170, 56]),
   }),
   naver: Object.freeze({
